1f136c763acd6c26d8a732d22647083394b66490
Jonathan Ott <jonathan.ott1994@gmail.com>
2019-07-08 13:14:05 +0200

    Update function descriptions

--- docsrc/content/IMzIODataReader.fsx
+++ docsrc/content/IMzIODataReader.fsx
@@ -2,6 +2,7 @@
 
 #r @"../../../MzIO/src/MzIO/bin/Release/net45/MzIO.dll"
 #r @"../../../MzIO/src/MzIO.MzML/bin/Release/net45/MzIO.MzML.dll"
+#r @"../../../MzIO/src/MzIO.Processing/bin/Release/net45/MzIO.Processing.dll"
 open MzIO
 open MzIO.IO.MzML.MzML
 let reader = new MzMLReader("samplestring")
@@ -10,56 +11,39 @@ let reader = new MzMLReader("samplestring")
 All readers in this project implement the interface of the IMzIODataReader. It contains abstract members 
 to interact with the data. Thus all readers can be used with the same processing functions.
 
-
 ###ReadMassSpectrum
 
 Uses a spectrum ID to get a specific spectrum contained by the model.
 *)
 reader.ReadMassSpectrum "spectrumID"
-
 (**
-###ReadMassSpectrum
-
 Returns a MassSpectrum which ID is equal to spectrumID or fails if no MassSpectrum has a fitting ID. This MassSpectrum does not contain intensity or m/z values but
 information about precusors, products and meta data information of the measured spectrum.
-
 
 ###ReadMassSpectra
 
 Uses a run ID to get all spectra contained by the model.
 *)
 reader.ReadMassSpectra "runID"
-
 (**
-###ReadMassSpectrum
-
 This function call returns a sequence of all spectra that are part of the same run. This doesn't include any intensity and m/z values but the metadata associated 
 with the spectra measurments.
-
 
 ###ReadSpectrumPeaks
 
 Uses a spectrum ID to get a Peak1DArray associated with a specific spectrum contained by the model.
 *)
 reader.ReadSpectrumPeaks "spectrumID"
-
 (**
-###ReadSpectrumPeaks
-
 This function call returns a Peak1DArray of the MassSpectrum. This class contains the intensity and m/z values and additional information, like the compression mode and data format
 of the values. By changing the compression mode the compression mode of the values will be altered when they get inserted into the SQLite database.
-
 
 ###ReadMassSpectrumAsync
 
 Uses a spectrum ID to get a specific spectrum contained by the model. This function is executed multiple times in parallel.
 *)
-
 reader.ReadMassSpectrumAsync "spectrumID"
-
 (**
-###ReadMassSpectrumAsync
-
 Returns a MassSpectrum which ID is equal to spectrumID or fails if no MassSpectrum has a fitting ID. This MassSpectrum does not contain intensity or m/z values but
 information about precusors, products and meta data information of the measured spectrum. The operation is executed in parellel to increase the speed.
 
@@ -67,12 +51,8 @@ information about precusors, products and meta data information of the measured 
 
 Uses a spectrum ID to get a Peak1DArray associated with a specific spectrum contained by the model. This function is executed multiple times in parallel.
 *)
-
 reader.ReadSpectrumPeaksAsync "spectrumID"
-
 (**
-
-###ReadSpectrumPeaksAsync
 This function call returns a Peak1DArray of the MassSpectrum. This class contains the intensity and m/z values and additional information, like the compression mode and data format
 of the values. By changing the compression mode the compression mode of the values will be altered when they get inserted into the SQLite database.
 The operation is executed in parellel to increase the speed.
@@ -82,14 +62,8 @@ The operation is executed in parellel to increase the speed.
 
 The functions which are required to read a Chromatogram are not implemented yet.
 *)
-
 reader.ReadChromatograms            "runID"
 reader.ReadChromatogram             "chromatogramID"
 reader.ReadChromatogramPeaks        "chromatogramID"
-reader.
 reader.ReadChromatogramAsync        "chromatogramID"
 reader.ReadChromatogramPeaksAsync   "chromatogramID"
-
-(**
-The functions which are required to read a Chromatogram are not implemented yet.
-*)
