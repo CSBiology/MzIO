43fa03596b0ee4fd328f6268a4b976e9b2c9999e
Jonathan Ott <jonathan.ott1994@gmail.com>
2019-07-16 11:23:24 +0200

    Add interface placeholder to MzMLWriter

---
+++ docsrc/content/IMzIODataWriter.fsx
@@ -0,0 +1,8 @@
+//(***hide***)
+
+//#r @"../../../MzIO/src/MzIO/bin/Release/net45/MzIO.dll"
+//#r @"../../../MzIO/src/MzIO.MzML/bin/Release/net45/MzIO.MzML.dll"
+//#r @"../../../MzIO/src/MzIO.Processing/bin/Release/net45/MzIO.Processing.dll"
+//open MzIO
+//open MzIO.IO.MzML
+//let writer = new MzMLWriter ("samplestring") :> MzIO.IO.IMzIODataWriter
